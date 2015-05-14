f3cd86812b6c80224f2f0069d504509d36f6cdd0
Graham Allan <graham.allan@timgroup.com>
2015-05-14 12:51:24 +0100

    Add test cases for joda time's local date.

--- build.sbt
+++ build.sbt
@@ -4,4 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.2"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.5" % "test"
+
+libraryDependencies += "joda-time" %  "joda-time" % "2.7"
+
+libraryDependencies += "org.joda" %  "joda-convert" % "1.7"
--- src/test/scala/MacroCompileErrorSpec.scala
+++ src/test/scala/MacroCompileErrorSpec.scala
@@ -1,7 +1,14 @@
 
+import java.time.LocalDate
+
 import org.scalatest.{DiagrammedAssertions, FunSpec}
 
 class SomeOtherClass(field: Int)
+
+object SomeOtherClass {
+  def create(field: Int) = new SomeOtherClass(field)
+}
+
 case class MyCaseClass(stringyThing: String, something: SomeOtherClass)
 
 class MacroCompileErrorSpec extends FunSpec with DiagrammedAssertions {
@@ -12,6 +19,19 @@ class MacroCompileErrorSpec extends FunSpec with DiagrammedAssertions {
 
       assert(something == MyCaseClass("b", new SomeOtherClass(2)))
     }
+
+    it("should not fail to compile when comparing to a case class created with a method") {
+      val something = MyCaseClass("a", new SomeOtherClass(1))
+
+      assert(something == MyCaseClass("b", SomeOtherClass.create(2)))
+    }
+
+    it("should not fail to compile when comparing to a java class created with static factory method") {
+      val myLocalDate = LocalDate.parse("2015-05-14")
+
+      assert(myLocalDate == LocalDate.parse("2015-05-14"))
+    }
+
   }
 
 }
